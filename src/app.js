9369ee3d3ab313e92a8e1d3bcc5b5ba6acc95927
SKuznetzov <s-kuznecov100@rambler.ru>
2022-03-03 12:21:44 +0300

    function toggleButton

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
+
+
